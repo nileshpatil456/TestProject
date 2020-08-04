fa65db6608743db4ede380b70eda3318a4b677d0
19137 <19137@PULT-19137.corp.hexaware.com>
2020-08-04 18:30:40 +0530

    Second Script

---
+++ Test/src/TestPackage/SecondScript.java
@@ -0,0 +1,17 @@
+package TestPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SecondScript {
+	
+	public static void main (String [] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\19137\\Desktop\\Selenium\\ChromeDriver\\");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com/");
+	driver.findElement(By.name("q")).sendKeys("SecondScript");
+		
+	}
+
+}
